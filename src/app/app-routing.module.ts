bfa20422109744266efcbd0fd2ecf52f8bc9add2
StjepanDev <stjepansuhic@yahoo.com>
2022-12-30 14:19:10 +0100

    quill installation

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,13 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { CreateBlogComponent } from './pages/create-blog/create-blog.component';
+import { HomeComponent } from './pages/home/home.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {path:'',pathMatch:'full',redirectTo:'home'},
+  {path:'home',component:HomeComponent},
+  {path:'create',component:CreateBlogComponent}
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,15 +4,32 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { TopbarComponent } from './components/topbar/topbar.component';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+
+import { MatButtonModule } from '@angular/material/button';
+import { MatFormFieldModule } from '@angular/material/form-field'; 
+import { MatInputModule } from '@angular/material/input'; 
+
+import { HomeComponent } from './pages/home/home.component';
+import { CreateBlogComponent } from './pages/create-blog/create-blog.component'; 
+
+import { QuillModule } from 'ngx-quill'
 
 @NgModule({
   declarations: [
     AppComponent,
-    TopbarComponent
+    TopbarComponent,
+    HomeComponent,
+    CreateBlogComponent
   ],
   imports: [
+    QuillModule.forRoot(),
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    BrowserAnimationsModule,
+    MatButtonModule,
+    MatFormFieldModule,
+    MatInputModule
   ],
   providers: [],
   bootstrap: [AppComponent]
